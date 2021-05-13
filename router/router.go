c50e133f210981ef9fa5812e85833a4705776afd
何涛 <hetao.kawaii@bytedance.com>
2021-05-13 16:24:54 +0800

    change cors config

--- router/router.go
+++ router/router.go
@@ -17,7 +17,7 @@ func InitRouter() {
 
 	v1 := route.Group("/api/v1")
 	{
-		v1.GET("/", handler.HandleRoot)
+		v1.Any("/", handler.HandleRoot)
 		v1.POST("/search", handler.HandleSearch)
 		v1.POST("/query", handler.HandleQuery)
 		v1.POST("/login", handler.HandleLogin)
